28584476be98e5535c33b6c1a2698c9c9d417577
Buvan <buvan.ece@hotmail.com>
2018-03-26 00:04:09 +0530

    Quick sort and Merge sort added

--- Sorting_Algorithms/SortingAlgorithmClass.cpp
+++ Sorting_Algorithms/SortingAlgorithmClass.cpp
@@ -26,7 +26,7 @@ void SortingAlgorithmClass::printsortedArray(int * inputArray, int arraySize)
 	cout << endl;
 }
 
-//Bubble Sorting
+//Bubble Sort
 
 int * SortingAlgorithmClass::BubbleSort(int * inputArray, int arraySize)
 {
@@ -72,6 +72,7 @@ int * SortingAlgorithmClass::SelectionSort(int * inputArray, int arraySize)
 		{
 			if (inputArray[j] < inputArray[minValue])
 				minValue = j;
+			printsortedArray(inputArray, arraySize);
 		}
 
 		swap(&inputArray[minValue], &inputArray[i]);
@@ -88,18 +89,18 @@ int * SortingAlgorithmClass::SelectionSort(int * inputArray, int arraySize)
 
 int * SortingAlgorithmClass::InsertionSort(int * inputArray, int arraySize)
 {
-	int i, j, insert;
+	int i, j;
 	
 	for (i = 1; i < arraySize; i++)
 	{
-		insert = inputArray[i];
-		cout << "Iteration " << i << " Starts" << endl << "-----------------------" << endl;
-		for (j = i - 1; j >= 0 && inputArray[j] > insert; j--)
+		cout << "Iteration " << i-1 << " Starts" << endl << "-----------------------" << endl;
+		j = i;
+		while (j > 0 && inputArray[j] < inputArray[j - 1])
 		{
-			inputArray[j+1] = inputArray[j];
+			swap(&inputArray[j], &inputArray[j - 1]);
+			j--;
 			printsortedArray(inputArray, arraySize);
 		}
-		inputArray[j+1] = insert;
 		printsortedArray(inputArray, arraySize);
 		cout << "Iteration End" << endl << "-----------------------" << endl;
 	}
@@ -109,11 +110,83 @@ int * SortingAlgorithmClass::InsertionSort(int * inputArray, int arraySize)
 	return inputArray;
 }
 
-int * SortingAlgorithmClass::MergeSort(int * inputArray, int arraySize)
+int * SortingAlgorithmClass::MergeSort(int * inputArray, int first, int last)
 {
-	return nullptr;
+	if (first < last)
+	{
+		int middle = (first + last) / 2;
+		MergeSort(inputArray, first, middle);
+		MergeSort(inputArray, middle + 1, last);
+		Merge(inputArray, first, middle, last);
+	}
+	return inputArray;
 }
-int *SortingAlgorithmClass::QuickSort(int * inputArray, int arraytSize)
+void SortingAlgorithmClass::Merge(int * inputArray, int first, int middle, int last)
 {
-	return nullptr;
+	int i, j, k;
+	i = first;
+	j = middle + 1;
+	k = 0;
+	int temp[50];
+
+	while (i <= middle && j <= last) 
+	{
+		if (inputArray[i] < inputArray[j])
+		{
+			temp[k] = inputArray[i];
+			i++;
+			k++;
+		}
+		else
+		{
+			temp[k] = inputArray[j];
+			j++;
+			k++;
+		}
+	}
+	while (i <= middle)
+	{
+		temp[k] = inputArray[i];
+		i++; k++;
+	}
+	while (j <= last)
+	{
+		temp[k] = inputArray[j];
+		j++; k++;
+	}
+	for (i = first; i <= last; i++)
+	{
+		inputArray[i] = temp[i - first];
+	}
+	cout << '\n' << "Merge Sorted Array is..." << endl;
+	printsortedArray(inputArray, last + 1);
+}
+int *SortingAlgorithmClass::QuickSort(int * inputArray, int left, int right)		// pivot Selection : last element
+{
+	if (left < right) 
+	{
+		int pivot_index = Partition(inputArray, left, right);
+
+		QuickSort(inputArray, left, pivot_index - 1);
+		QuickSort(inputArray, pivot_index + 1, right);
+	}
+	return inputArray;
+}
+
+int SortingAlgorithmClass::Partition(int * inputArray, int left, int right)
+{
+	int j, i = left - 1;
+	int pivot = inputArray[right];
+
+	for (j = left; j < right; j++)
+	{
+		if (inputArray[j] <= pivot)
+		{
+			i++;
+			swap(&inputArray[i], &inputArray[j]);
+		}
+	}
+	swap(&inputArray[i + 1], &inputArray[j]);
+	//printsortedArray(inputArray, right + 1);
+	return (i + 1);
 }
--- Sorting_Algorithms/SortingAlgorithmClass.h
+++ Sorting_Algorithms/SortingAlgorithmClass.h
@@ -14,6 +14,8 @@ public:
 	int * BubbleSort(int * inputArray, int arraySize);
 	int * SelectionSort(int *inputArray, int arraySize);
 	int * InsertionSort(int * inputArray, int arraySize);
-	int * MergeSort(int * inputArray, int arraySize);
-	int * QuickSort(int * intputArray, int arraySize);
+	int * MergeSort(int * inputArray, int first, int last);
+	void Merge(int *inputArray, int first, int middle, int last);
+	int * QuickSort(int * intputArray, int left,int right);
+	int Partition(int * inputArray, int left, int right);
 };
